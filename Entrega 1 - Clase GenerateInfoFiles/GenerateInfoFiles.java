c4a3e068b6cda5c9e8ce72d44f32b61c5ee384a4
shadow4646 <shadow4646@github.com>
2026-03-24 22:48:12 -0500

    Corregir estructura del proyecto Eclipse - Entrega 1
    - Mover GenerateInfoFiles.java a carpeta correcta
    - Agregar archivos de proyecto Eclipse (.project, .classpath)
    - Agregar .gitignore para excluir .class y .txt generados

--- Entrega 1 - Clase GenerateInfoFiles/GenerateInfoFiles.java
+++ Entrega 1 - Clase GenerateInfoFiles/GenerateInfoFiles.java
@@ -1,4 +1,4 @@
-import java.io.BufferedWriter;
+﻿import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
@@ -342,3 +342,4 @@ public class GenerateInfoFiles {
         return true;
     }
 }
+
